24c16422604d9fe1c60fead15866927f23344172
OIS\eygerman <eygerman@asuproject.ru>
2024-05-22 20:25:44 +0500

    prometheus +

---
+++ src/VirtualAssistant/EmulatePost1.ps1
@@ -0,0 +1,3 @@
+while($true) {
+    curl -Method POST http://arch.homework/otusapp/eygerman/performance/MockPost1
+}
---
+++ src/VirtualAssistant/VirtualAssistant.API/Controllers/PerformanceController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace VirtualAssistant.API.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class PerformanceController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> MockGet1(CancellationToken token)
+        {
+            if (new Random().NextDouble() > 0.95)
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            
+            await Task.Delay(new Random().Next(50, 100), token);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MockGet2(CancellationToken token)
+        {
+            if (new Random().NextDouble() > 0.98)
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+
+            await Task.Delay(new Random().Next(100, 150), token);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MockPost1(CancellationToken token)
+        {
+            if (new Random().NextDouble() > 0.95)
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+
+            await Task.Delay(new Random().Next(150, 200), token);
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> MockPost2(CancellationToken token)
+        {
+            if (new Random().NextDouble() > 0.9)
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+
+            await Task.Delay(new Random().Next(800, 1500), token);
+
+            return Ok();
+        }
+    }
+}
